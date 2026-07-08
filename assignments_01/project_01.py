311d2510ea15d1321e8843d9137eaba3e9e33b83
Alena Dudko <e.v.dudko@gmail.com>
2026-07-07 20:45:48 -0500

    Task 3: Visual Exploration

--- assignments_01/project_01.py
+++ assignments_01/project_01.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 from prefect import task, flow
 from prefect.logging import get_run_logger
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #Task 1: Load Multiple Years of Data
 @task(retries=3, retry_delay_seconds=2)
@@ -61,10 +63,68 @@ def descriptive_statistics(df):
     mean_by_region = df.groupby("Regional indicator")["Happiness score"].mean()
     logger.info(f"Mean happiness score by region:\n{mean_by_region}")
 
+
+#Task 3: Visual Exploration
+@task
+def visual_exploration(df):
+    logger = get_run_logger()
+    current_folder = os.path.dirname(__file__)
+    output_folder = os.path.join(current_folder, "outputs")
+
+    #Histogram of all happiness scores across all years
+    plt.figure(figsize=(8, 5))
+    plt.hist(df["Happiness score"], bins=20, edgecolor="black")
+    plt.title("Happiness Score Distribution Across All Years")
+    plt.xlabel("Happiness Score")
+    plt.ylabel("Frequency")
+
+    plt.savefig(os.path.join(output_folder, "happiness_histogram.png"))
+    plt.close()
+
+    logger.info("Saved happiness_histogram.png")
+
+    #Boxplot comparing happiness score distributions across years (one box per year).
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(data=df, x="Year", y="Happiness score")
+    plt.title("Happiness Scores by Year")
+    plt.xlabel("Year")
+    plt.ylabel("Happiness Score")
+
+    plt.tight_layout()
+
+    plt.savefig(os.path.join(output_folder, "happiness_by_year.png"))
+    plt.close()
+
+    logger.info("Saved happiness_by_year.png")
+
+
+    #Scatter plot showing the relationship between GDP per capita and happiness score.
+    plt.figure(figsize=(8, 6))
+    plt.scatter(df["GDP per capita"], df["Happiness score"])
+    plt.title("GDP per Capita vs. Happiness Score")
+    plt.xlabel("GDP per Capita")
+    plt.ylabel("Happiness Score")
+
+    plt.tight_layout()
+
+    plt.savefig(os.path.join(output_folder, "gdp_vs_happiness.png"))
+    plt.close()
+
+    logger.info("Saved gdp_vs_happiness.png")
+    
+    
+    
+
+
+
+
+
+
 @flow
 def happiness_pipeline():
     df = load_data()
     descriptive_statistics(df)
+    visual_exploration(df)
 
            
 if __name__ == "__main__":
